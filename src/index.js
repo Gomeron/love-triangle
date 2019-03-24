c2fed0d4ec140ed892798d833edc53ba4f93d732
Gomeron <zhuckkirill@yandex.ru>
2019-03-24 19:49:47 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+    let count = 0;
+    for (let i = 0, lenght = preferences.length; i < lenght; i++) {
+        const cond1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+        const cond2 = i + 1 !== preferences[i];
+        if (cond1 && cond2) {
+            count++;
+        }
+    }
+    return count / 3;
+};
